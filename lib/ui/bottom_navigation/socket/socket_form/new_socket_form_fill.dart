a3644eedf7cff4fe83da987a9564b1af57eeefd2
Piyush <piyushlodaliya@gmail.com>
2023-08-07 11:32:27 +0530

    New branch

--- lib/ui/bottom_navigation/socket/socket_form/new_socket_form_fill.dart
+++ lib/ui/bottom_navigation/socket/socket_form/new_socket_form_fill.dart
@@ -24,6 +24,7 @@ class _NewSocketFormScreenState extends State<NewSocketFormScreen> {
         child: SizedBox(
           child: Column(
             children: [
+
               TextFormField()
             ],
           ),
